04be9e1a26fc7665f59f88f7e0c1424ca6e57950
HumphreyJ <joshhumphrey1@gmail.com>
2020-09-08 17:28:38 -0500

    modified knex file

--- knexfile.js
+++ knexfile.js
@@ -22,5 +22,26 @@ module.exports = {
        max: 10
       }
     },
+    production: {
+      client: 'postgresql',
+      connection: {
+        database: process.env.PGDATABASE,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        host: process.env.PGHOST,
+        port: process.env.PGPORT,
+      },
+      migrations: {
+        directory: './db/migrations'
+      },
+      seeds : {
+        directory: './db/seeds'
+      },
+      pool: {
+       min: 2,
+       max: 10
+      }
+    },
   };
+  }
   
