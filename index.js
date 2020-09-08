4ad8b3ccc4442f2a31d93207969a139b3d121e75
HumphreyJ <joshhumphrey1@gmail.com>
2020-09-08 17:17:53 -0500

    the heroku battle continues

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000
 
 server.use('/api', routes)
 
+server.get("/", (req, res) => {
+	res.send(`
+		<h1>This is a test!</h1>
+	`);
+});
+
 server.listen(PORT, () =>
 			console.log(`Listening on port ${PORT}`)
 )
